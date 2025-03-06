a9c220dacc0af080f330d36dc558e7d69ac676a9
Danielle Pashayan <daniellepashayan@gmail.com>
2025-03-05 19:46:41 -0500

    changed port

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ async def import_data(request: Request, api_key: Optional[str] = Header(None)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=3111)
